50cefcbf46e138f98fbe548685cd4d48118100e7
arshigtx <arshi@Arshis-MacBook-Pro-2.local>
2019-07-20 22:21:33 -0400

    update testing

--- src/components/layout/mainnav.vue
+++ src/components/layout/mainnav.vue
@@ -1,17 +1,17 @@
 <template lang="html">
   <div>
    <nav class="menu-nav">
-     <router-link to="/"> <li>Dashboard </li></router-link>
-     <router-link to="/daybook"><li>Daybook</li></router-link>
-     <router-link to="/contacts" ><li>Contacts</li></router-link>
-     <router-link to="/projects"><li>Projects</li></router-link>
+     <router-link to="/"> <li>DASBHOARD </li></router-link>
+     <router-link to="/daybook"><li>TIMEBOOK</li></router-link>
+     <router-link to="/contacts" ><li>CONTACTS</li></router-link>
+     <router-link to="/projects"><li>PROJECTS</li></router-link>
      <router-link to="/wip"><li>WIP</li></router-link>
-      <router-link to="/invoice"> <li>Invoice</li></router-link>
-      <router-link to="/reports"> <li> Reports</li></router-link>
-      <router-link to="/accounting" > <li> Accounting</li>
-      </router-link><router-link to="/finplan" ><li>Finplan</li></router-link>
-      <router-link to="/traders" ><li>Trader's Edge</li></router-link>
-      <router-link to="/setup" ><li>Setup</li></router-link>
+      <router-link to="/invoice"> <li>INVOICE</li></router-link>
+      <router-link to="/reports"> <li> REPORTS</li></router-link>
+      <router-link to="/accounting" > <li> ACCOUNTING</li>
+      </router-link><router-link to="/finplan" ><li>FINPLAN</li></router-link>
+      <router-link to="/traders" ><li>TRADER'S EDGE</li></router-link>
+      <router-link to="/setup" ><li>SETUP</li></router-link>
     </nav>
   </div>
 </template>
